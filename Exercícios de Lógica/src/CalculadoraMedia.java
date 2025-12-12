1026dbfc99eb975dd534ab93b82f37f239a72ba5
Maria Eduarda Albuquerque <dudinhazilda17@gmail.com>
2025-12-12 15:31:18 -0300

    Add files via upload

---
+++ Exercícios de Lógica/src/CalculadoraMedia.java
@@ -0,0 +1,19 @@
+package listaCaderno;
+import java.util.Scanner;
+public class CalculadoraMedia {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int n1, n2, media;
+
+		System.out.println("Digite a primeira nota do aluno:");
+		n1 = ler.nextInt();
+
+		System.out.println("Digite a segunda nota do aluno:");
+		n2 = ler.nextInt();
+
+		media = (n1+n2)/2;
+		
+		System.out.println("A media do aluno foi "+ media);
+		ler.close();		
+	}
+}
