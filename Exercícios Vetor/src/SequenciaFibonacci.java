e61fc33ede80097daefbfb7526e0dd19aa7ada5c
Maria Eduarda Albuquerque <dudinhazilda17@gmail.com>
2025-12-12 15:54:12 -0300

    Add files via upload

---
+++ Exercícios Vetor/src/SequenciaFibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SequenciaFibonacci {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n, i, a=0, b=1, c;
+		
+		System.out.println("Entre com o número de termos: ");
+		n = in.nextInt();
+
+		for(i=1; i<=n; i++) {
+			System.out.print(b + " ");
+			c = a+b;
+			a= b;
+			b = c;
+		}
+	}
+}
